7d2d5a7e564a7457d15a58ed06233ae7c155906d
macbook <macbook@jungee.local>
2023-03-02 10:50:29 +0900

    Add: mixin에 내용 추가 -이름 수정

--- src/styles/mixin.js
+++ src/styles/mixin.js
@@ -6,3 +6,26 @@ export const flex = (jc = 'center', ai = 'center', fd = 'row') => css`
   align-items: ${ai};
   flex-direction: ${fd};
 `;
+
+export const linkStyle = css`
+  color: black;
+  text-decoration: none;
+`;
+
+export const carouselBtn = css`
+  position: absolute;
+  top: 38%;
+  width: 45px;
+  height: 45px;
+  background-color: white;
+  border-radius: 50%;
+  border: 1px solid #eeeeee;
+  box-shadow: 0 2px 5px rgb(63 71 77 / 15%);
+  text-align: center;
+  font-size: 35px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #eeeeee;
+  }
+`;

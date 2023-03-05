e96ba81a08038296882c93a04550e3e15f0eb0fa
leejonghyuk <as13ljh@gmail.com>
2023-03-06 04:22:25 +0900

    Add: homewarming page 이미지 plus버튼, 댓글기능, 스크랩기능 추가 -캐러셀 수정

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const BASE_URl_S = 'http://10.58.52.176:8000';
-const BASE_URl_J = 'http://10.58.52.210:8000';
+const BASE_URl_J = 'http://10.58.52.228:8000';
 
 export const API = {
   LOGIN: `${BASE_URl_S}/users/kakaoLogin`,
@@ -12,3 +12,5 @@ export const API = {
   COMMENTS: `${BASE_URl_S}/comments`,
   REVIEWS: `${BASE_URl_J}/reviews`,
 };
+
+export default API;
--- src/pages/HomeWarming/HomeWarming.jsx
+++ src/pages/HomeWarming/HomeWarming.jsx
@@ -1,35 +1,108 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import * as S from './HomeWarming.Style';
+import API from '../../config/config';
 import Comment from './Comment/Comment';
 import SideBar from './SideBar/SideBar';
+import ProductModal from './ProductModal/ProductModal';
 
 export default function HomeWarming() {
+  const [homeWarmingData, setHomeWramingData] = useState([{}]);
+  const [commentData, setCommentData] = useState([{}]);
+  const [showPlusBtn, setShowPlusBtn] = useState(false);
+  const [showProductModal, setShowProductModal] = useState(false);
+  const [userInfo, setUserInfo] = useState([{}]);
+
+  const params = useParams();
+
+  useEffect(() => {
+    fetch(`${API.COMMENTS}/post/${params.id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: localStorage.getItem('token'),
+      },
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setCommentData(data.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`${API.POSTS}/${params.id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: localStorage.getItem('token'),
+      },
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setHomeWramingData(data.data);
+        setUserInfo(data.user);
+      });
+  }, []);
+
+  const handlePlusBtnHover = () => setShowPlusBtn(true);
+
+  const handlePlusBtnLeave = () => {
+    setShowPlusBtn(false);
+    setShowProductModal(false);
+  };
+
+  const handlePlusBtnClick = () => {
+    setShowProductModal(prev => !prev);
+  };
+
+  const pixelRow = homeWarmingData.productInfo?.pixelRow;
+  const pixelColumn = homeWarmingData.productInfo?.pixelColumn;
+
   return (
     <div>
-      <S.TitleImg src="/images/HomeWarming/room.jpg" alt="title img" />
+      <S.TitleImg src={homeWarmingData.postImageUrl} alt="title img" />
       <S.MainTextWrap>
-        <SideBar />
+        <SideBar homeWarmingData={homeWarmingData} commentData={commentData} />
         <S.TitleWrap>
           <S.CategoryTitle>온라인 집들이</S.CategoryTitle>
-          <S.Title>포근한 집을 소개합니다.</S.Title>
+          <S.Title>{homeWarmingData.title}</S.Title>
           <S.UserInfoWrap>
-            <S.UserImg src="/images/HomeWarming/userImg.png" alt="userimg" />
-            <S.UserId>user1</S.UserId>
+            <S.UserImg src={homeWarmingData.profileImage} alt="userimg" />
+            <S.UserId>{homeWarmingData.nickname}</S.UserId>
           </S.UserInfoWrap>
         </S.TitleWrap>
-        <S.HomeImg src="/images/HomeWarming/room.jpg" alt="home img" />
-        <S.Explanation>
-          아직 입주한 지 이제 1달 정도 밖에 안되어서 많이 부족하지만, 예쁘게
-          봐주시면 감사하겠습니다 :)
-        </S.Explanation>
+        <div
+          onMouseEnter={handlePlusBtnHover}
+          onMouseLeave={handlePlusBtnLeave}
+        >
+          <S.HomeImg src={homeWarmingData.postImageUrl} alt="home img" />
+
+          {showPlusBtn && (
+            <S.MarkerBox pixelRow={pixelRow} pixelColumn={pixelColumn}>
+              <S.PlusBtn onClick={handlePlusBtnClick}>+</S.PlusBtn>
+              {showProductModal && (
+                <ProductModal homeWarmingData={homeWarmingData} />
+              )}
+            </S.MarkerBox>
+          )}
+        </div>
+        <S.Explanation>{homeWarmingData.description}</S.Explanation>
         <S.UserCard>
-          <S.UserImg src="/images/HomeWarming/userImg.png" alt="userimg" />
+          <S.UserImg src={homeWarmingData.profileImage} alt="userimg" />
           <S.TextWrap>
-            <S.UserName>user1</S.UserName>
+            <S.UserName>{homeWarmingData.nickname}</S.UserName>
             <p>안녕하세요!</p>
           </S.TextWrap>
         </S.UserCard>
-        <Comment />
+        <Comment
+          commentData={commentData}
+          setCommentData={setCommentData}
+          userInfo={userInfo}
+        />
       </S.MainTextWrap>
     </div>
   );

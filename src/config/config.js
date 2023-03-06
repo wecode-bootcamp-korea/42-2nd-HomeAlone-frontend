cf876a99e8e89b42865037ce80d697339dba7546
soomminglee <soomming.lee@gmail.com>
2023-03-06 11:15:42 +0900

    Add : 공용 API 관리 파일 생성

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,13 @@
-const BASE_URl = '';
+const BASE_URl_S = 'http://10.58.52.189:8000';
+const BASE_URl_J = 'http://10.58.52.228:8000';
+
+export const API = {
+  LOGIN: `${BASE_URl_J}/users/kakaoLogin`,
+  POSTS: `${BASE_URl_S}/posts`,
+  PRODUCTS: `${BASE_URl_J}/products`,
+  CARTS: `${BASE_URl_S}/carts`,
+  PAYMENTS: `${BASE_URl_J}/payments`,
+  SCRAPS: `${BASE_URl_S}/scraps`,
+  COMMENTS: `${BASE_URl_S}/comments`,
+  REVIEWS: `${BASE_URl_J}/reviews`,
+};
